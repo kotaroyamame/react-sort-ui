9b2fd412677209caa986b20b5cad89863f03f63f
andhy <>
2023-08-28 23:12:12 +0900

    commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,35 @@
-const postcss = require("rollup-plugin-postcss");
-const typescript = require("@rollup/plugin-typescript");
-const peerDepsExternal = require("rollup-plugin-peer-deps-external");
-const {nodeResolve} = require('@rollup/plugin-node-resolve');
-const commonjs = require("@rollup/plugin-commonjs");
+const postcss = require('rollup-plugin-postcss')
+const typescript = require('@rollup/plugin-typescript')
+const peerDepsExternal = require('rollup-plugin-peer-deps-external')
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const commonjs = require('@rollup/plugin-commonjs')
 module.exports = {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: {
-    dir: "dist",
-    format: "cjs",
+    dir: 'dist',
+    format: 'esm'
   },
   plugins: [
     peerDepsExternal(),
-    nodeResolve(
-{
-custonResolveOptions:{
-	moduleDirectory:"node_modules",
-},
-extensions:[".js",".jsx",".ts",".tsx",".css",".mjs",".cjs"]}
-),
-    typescript(),
-    commonjs(),
-    postcss({
-      modules: true,
+    nodeResolve({
+      browser: true,
+      custonResolveOptions: {
+        moduleDirectory: 'node_modules'
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.css', '.mjs', '.cjs']
     }),
-  ],
-};
+    typescript(),
+    commonjs({
+      namedExports: {
+        'node_modules/react/index.js': [
+          'createFactory',
+          'Component',
+          'createElement'
+        ]
+      }
+    }),
+    postcss({
+      modules: true
+    })
+  ]
+}
